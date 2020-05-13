56066f65d43a9d09c99b2c738d7eda0d1dccacfc
Conor Sands <sandscon@msu.edu>
2020-05-13 10:50:47 -0400

    Adding newest changes

--- js/receiver.js
+++ js/receiver.js
@@ -1,7 +1,29 @@
+const NAMESPACE = 'urn:x-cast:com.sandscon.euchre';
+
 const context = cast.framework.CastReceiverContext.getInstance();
 
-// Update style using javascript
-let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("https://images-na.ssl-images-amazon.com/images/I/51an3ywP53L.jpg")');
+// Chromecast device is disconnected from sender app.
+context.addEventListener(
+    cast.framework.system.EventType.SENDER_DISCONNECTED, (event) => {
+        window.close();
+    });
+
+// Receives messages from sender app. The message is a comma separated string
+// where the first substring indicates the trump suit, the second substring is
+// the first team's current score, and the third substring is the second team's
+// score.
+context.addCustomMessageListener(NAMESPACE, (event) => {
+    let message = event.data.split(',');
+
+    // Get the substrings as separate variables.
+    let trumpSuit = message[0];
+    let teamOneScore = message[1];
+    let teamTwoScore = message[2];
+
+    // Set the picture on screen depending on the suit sent. If null, make the
+    // image transparent.
+
+    // Set the team scores.
+});
 
 context.start();
