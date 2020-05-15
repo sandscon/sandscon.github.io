de0ac30e1f230d0206a90d4875bdb35c45977749
Conor Sands <sandscon@msu.edu>
2020-05-14 22:55:36 -0400

    IDK

--- js/receiver.js
+++ js/receiver.js
@@ -12,7 +12,7 @@ context.addEventListener(
 // where the first substring indicates the trump suit, the second substring is
 // the first team's current score, and the third substring is the second team's
 // score.
-context.addCustomMessageListener(NAMESPACE, event => {
+context.addCustomMessageListener(NAMESPACE, (event) => {
     console.log(event);
 
     message = event.data.split(';');
