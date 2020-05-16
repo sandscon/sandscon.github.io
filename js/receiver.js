979dca267d8351d9b8d2ca0f2e7a41e143f8beae
Conor Sands <sandscon@msu.edu>
2020-05-15 23:27:34 -0400

    Adding images and subsequent logic to suits on screen

--- js/receiver.js
+++ js/receiver.js
@@ -1,4 +1,18 @@
 const NAMESPACE = 'urn:x-cast:com.sandscon.euchre';
+const SPADE = "SPADE";
+const SPADE_IMAGE_FILEPATH = "../images/spade_final.png";
+const HEART = "HEART";
+const HEART_IMAGE_FILEPATH = "../images/heart_final.png";
+const DIAMOND = "DIAMOND";
+const DIAMOND_IMAGE_FILEPATH = "../images/diamond_final.png";
+const CLUB = "CLUB";
+const CLUB_IMAGE_FILEPATH = "../images/club_final.png";
+const TRUMP_JSON_KEY = "trump";
+const TRUMP_TEAM_ONE_SCORE_KEY = "teamOneScore";
+const TRUMP_TEAM_TWO_SCORE_KEY = "teamOneScore";
+const SUIT_IMAGE_ID = "suit-image";
+const TEAM_ONE_SCORE_ID = "team-one-score";
+const TEAM_TWO_SCORE_ID = "team-two-score";
 
 const context = cast.framework.CastReceiverContext.getInstance();
 
@@ -14,34 +28,50 @@ context.addEventListener(
 // score.
 context.addCustomMessageListener(NAMESPACE, (event) => {
 
+    // Get the data from the event.
     let data = event.data;
 
     // Get the substrings as separate variables.
-    let trumpSuit = data["trump"];
-    let teamOneScore = data["teamOneScore"];
-    let teamTwoScore = data["teamTwoScore"];
+    let trumpSuit = data[TRUMP_JSON_KEY];
+    let teamOneScore = data[TRUMP_TEAM_ONE_SCORE_KEY];
+    let teamTwoScore = data[TRUMP_TEAM_TWO_SCORE_KEY];
 
-    // Set the picture on screen depending on the suit sent. If null, make the
-    // image transparent.
+    // Set the picture on screen depending on the suit sent. If undefined, make 
+    // the image transparent.
     switch(trumpSuit) {
-        case "SPADE":
+        case SPADE:
+            changeImage(SPADE_IMAGE_FILEPATH);
             break;
-        case "HEART":
+        case HEART:
+            changeImage(HEART_IMAGE_FILEPATH);
             break;
-        case "DIAMOND":
+        case DIAMOND:
+            changeImage(DIAMOND_IMAGE_FILEPATH);
             break;
-        case "CLUB":
+        case CLUB:
+            changeImage(CLUB_IMAGE_FILEPATH);
             break;
         default:
-            // Make the image invisible because a trump suit has not been selected.
+            document.getElementById().style.opacity = 0;
             break;
     }
 
     // Set the team scores.
-    document.getElementById("team-one-score").innerHTML = teamOneScore;
-    document.getElementById("team-two-score").innerHTML = teamTwoScore;
+    document.getElementById(TEAM_ONE_SCORE_ID).innerHTML = teamOneScore;
+    document.getElementById(TEAM_TWO_SCORE_ID).innerHTML = teamTwoScore;
 });
 
-document.getElementById("team-one-score").innerHTML = "3";
-
 context.start();
+
+/**
+ * Changes the suit image based on a filepath.
+ * 
+ * @param {string} image_filepath Filepath of image.
+ */
+function changeImage(image_filepath) {
+    // Set the image opacity to 100.
+    document.getElementById(SUIT_IMAGE_ID).style.opacity = 100;
+
+    // Change the image based on the passed-in filepath.
+    document.getElementById(SUIT_IMAGE_ID).src = image_filepath;
+}
